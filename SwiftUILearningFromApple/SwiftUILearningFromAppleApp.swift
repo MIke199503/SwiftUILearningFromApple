935c8ee13c24f87af2ec77a34c495cfd4e9b6574
Mike19950326 <zth19950326@yahoo.com>
2023-01-12 10:30:57 +0800

    Initial Commit

---
+++ SwiftUILearningFromApple/SwiftUILearningFromAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningFromAppleApp.swift
+//  SwiftUILearningFromApple
+//
+//  Created by 蜡笔小新的动感超人Imac on 2023/1/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningFromAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
