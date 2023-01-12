fa794d7da58cca27d81661b0038f8590ce2c0144
Mike19950326 <zth19950326@yahoo.com>
2023-01-12 15:39:44 +0800

    first

---
+++ SwiftUILearningFromApple/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUILearningFromApple
+//
+//  Created by 蜡笔小新的动感超人Imac on 2023/1/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
